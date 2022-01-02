2e4aa39688658b70450d68517214a6f6d70c744e
DuckYoung Lee <q7y331xk@naver.com>
2022-01-02 12:32:13 +0900

    22-01-02

---
+++ to-do-app/src/App.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+import { Fragment } from "react/cjs/react.production.min";
+
+function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((curr) => [toDo, ...curr]);
+    setToDo("");
+  };
+  return (
+    <Fragment>
+      <h1>My To Dos {toDos.length}</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          value={toDo}
+          onChange={onChange}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add to do </button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </Fragment>
+  );
+}
+
+export default App;
